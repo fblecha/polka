899a32dd7991f669195086d5fb85caaa03b5cb93
Frank Blecha <frank@blecha.org>
2015-07-21 22:54:02 -0600

    WIP cli design

--- polka/main.go
+++ polka/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
-
+import (
+	"log"
+	"os"
+	"fmt"
+	"github.com/mitchellh/cli"
+)
 func main() {
 	fmt.Println("Let's Polka!")
+  c := cli.NewCLI("polka", "0.0.1")
+  c.Args = os.Args[1:]
+  c.Commands = map[string]cli.CommandFactory{
+      "foo": fooCommandFactory,
+      "bar": barCommandFactory,
+  }
+
+  exitStatus, err := c.Run()
+  if err != nil {
+      log.Println(err)
+  }
+
+  os.Exit(exitStatus)
 }
