2126aa5bf8054592af2ce740f3907ce15dec9ef1
Frank Blecha <frank@blecha.org>
2015-08-14 23:47:43 -0600

    refactored into template_helper

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"log"
 	"os"
-	"fmt"
-	"errors"
-	//"path/filepath"
-	"github.com/kardianos/osext"
 	"github.com/mitchellh/cli"
 )
 
 func main() {
-
 
 	templateDir, err := FindTemplateDir()
 	if err != nil {
@@ -36,29 +31,4 @@ func main() {
 	}
 
 	os.Exit(exitStatus)
-}
-
-func TestTemplateDir(dirName string) bool  {
-	log.Printf("looking in %v\n", dirName)
-	_, err := os.Stat(dirName);
-	return err == nil  //if err == true, we found the dir; it's false otherwise
-}
-
-func FindTemplateDir() (string, error) {
-	if folderPath, err := osext.ExecutableFolder(); err == nil {
-		log.Println(folderPath)
-
-		devTemplateDirName := fmt.Sprintf("%v/templates", folderPath)
-		brewTemplateDirName := fmt.Sprintf("%v/../share/templates", folderPath)
-
-		//maybe we're running this in dev mode, so templates could be at folderPath/templates
-		if TestTemplateDir(devTemplateDirName) {
-			return devTemplateDirName, nil
-		} else if TestTemplateDir(brewTemplateDirName) {
-			return brewTemplateDirName, nil
-		}
-	}
-	//we only reach this part if err != nil
-	return "", errors.New("unable to find templates dir")
-
 }
---
+++ template_helper.go
@@ -0,0 +1,32 @@
+package main
+import (
+  "log"
+  "fmt"
+  "os"
+  "errors"
+  "github.com/kardianos/osext"
+)
+func TemplateDirExists(dirName string) bool  {
+	log.Printf("looking in %v\n", dirName)
+	_, err := os.Stat(dirName);
+	return err == nil  //if err == true, we found the dir; it's false otherwise
+}
+
+func FindTemplateDir() (string, error) {
+	if folderPath, err := osext.ExecutableFolder(); err == nil {
+		log.Println(folderPath)
+
+		devTemplateDirName := fmt.Sprintf("%v/templates", folderPath)
+		brewTemplateDirName := fmt.Sprintf("%v/../share/templates", folderPath)
+
+		//maybe we're running this in dev mode, so templates could be at folderPath/templates
+		if TemplateDirExists(devTemplateDirName) {
+			return devTemplateDirName, nil
+		} else if TemplateDirExists(brewTemplateDirName) {
+			return brewTemplateDirName, nil
+		}
+	}
+	//we only reach this part if err != nil
+	return "", errors.New("unable to find templates dir")
+
+}
