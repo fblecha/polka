5211f5537ed2e8ba44ea05541d2d171d3cd00425
Frank Blecha <frank@blecha.org>
2015-08-17 21:31:51 -0600

    WIP starting to put together doc command

--- command/doc.go
+++ command/doc.go
@@ -1,16 +1,26 @@
 package command
 
 import (
-	"fmt"
 	"github.com/mitchellh/cli"
 	"log"
 	"strings"
-	//"text/template"
+	"os"
+	"fmt"
+	//"encoding/json"
+	//"io/ioutil"
+	"path/filepath"
 )
 
 type DocCommand struct {
 	Name string
 	Ui   cli.Ui
+}
+
+type ResourceConfig struct {
+	Name 			string
+	RAM	 			int //in MB
+	Timeout		int //in seconds
+	AWSRegion string
 }
 
 
@@ -23,10 +33,10 @@ Generates documentation for your polka app.
 If no subcommand is specified, polka documents everything.
 
 Subcommands:
-	endpoints - list out all the endpoints in this application
-	integrations -
-	arch	- document the architectur
-	resources - summary table of the resources requested for each lambda function
+	endpoints 	- list out all the endpoints in this application
+	integrations	-
+	architecture	-
+	resources 	- summary table of the resources requested for each lambda function
 `
 	return strings.TrimSpace(helpText)
 }
@@ -34,16 +44,64 @@ Subcommands:
 func (c *DocCommand) Run(args []string) int {
 
 	log.Printf("new command # of args = %v\n", len(args))
-	if len(args) < 1 {
-		fmt.Printf("%v", c.Help())
-		return 1
-	}
 
-	log.Fatal("doc command is currently just a stub")
+	/*
+		if the current working directory doesn't contain an app directory  (e.g. ./app/ exists),
+		then we fatal out.  Otherwise, let's iterate over everything.
+	*/
+	DocEndpoints()
+	DocIntegrations()
+	DocArchitecture()
+	DocResources()
 
 	return 0
 }
 
 func (c *DocCommand) Synopsis() string {
 	return "Generates documentation for your polka app"
+}
+
+func DocEndpoints() {
+	//check that we're in the app directory
+}
+
+func DocIntegrations() {
+
+}
+
+func DocArchitecture() {
+
+}
+
+
+
+//DocResources emits a summary table
+func DocResources() {
+	//let's just do endpoints initially
+	appDirectory, err := GetAppDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	location := fmt.Sprintf("%v/app", appDirectory)
+
+	var configLoader = func(path string, fileInfo os.FileInfo, _ error) (err error) {
+		if fileInfo, err := os.Lstat(path); err == nil {
+			if fileInfo.Name() == "lambda_config.json" {
+				log.Printf("found %v in %v", fileInfo.Name(), path)
+/*
+				var resouce ResourceConfig
+				var jsontype jsonobject
+				json.Unmarshal(file, &resource)
+				fmt.Printf("Results: %v\n", resource)
+*/
+			}
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	err = filepath.Walk(location, configLoader);
+	log.Println(err)
 }
