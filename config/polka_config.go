da242e868bc54bb7e107c6e2153b3c4c26046cde
Frank Blecha <frank@blecha.org>
2015-08-28 23:05:17 -0600

    updated to use homeplate

--- config/polka_config.go
+++ config/polka_config.go
@@ -7,12 +7,17 @@ type PolkaConfig struct {
 	Name string
 }
 
+func (p *PolkaConfig) Save() {
+	homeDir := "~/.polka"
+	homeplate.Save(homeDir, "~/.polka/polka_config.json", p, false)
+}
+
+
 //CreatePolkaHomeConfig creates ~/.polka if it doesn't exist already.
 //It then places the contents of PolkaConfig into it as ~/.polka/polka_config.json
 func CreatePolkaHomeConfigAsNeeded() {
-	homeDir := "~/.polka"
 	config := PolkaConfig{
 		Name: "polka",
 	}
-	homeplate.Save(homeDir, "~/.polka/polka_config.json", config, false)
+	config.Save()
 }
