c2d3a1fc120638ffa7dd291a967c28799b79081a
Frank Blecha <frank@blecha.org>
2015-08-31 07:57:09 -0600

    added message for not in app directory

--- command/helpers.go
+++ command/helpers.go
@@ -3,3 +3,19 @@ package command
 func GetAppDir() (string, error) {
 	return ".", nil //TODO
 }
+
+func NotInAppDirectoryMessage() string {
+	helpText := `
+polka expects to be run in an app directory created by 'polka new'.  E.g the following:
+
+(1) $ pwd
+~/code
+(2) $ polka new todo
+(3) $ cd todo
+(4) $ polka [command]
+
+You likely forgot step (3)
+
+`
+	return helpText
+}
